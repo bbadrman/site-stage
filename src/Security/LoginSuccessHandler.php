d5e6f2ce5c34f325cdb0651b7f52e4da7c440c55
dinachabil-cyber <dina.chabil@aksam-assurances.ma>
2026-02-17 16:50:22 +0100

    IMS-14: admin dashboard

---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $user = $token->getUser();
+        $roles = $user->getRoles();
+
+        if (in_array("ROLE_ADMIN", $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate("app_admin_dashboard"));
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate("app_home"));
+    }
+}
